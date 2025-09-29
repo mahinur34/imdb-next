3058c1582e368e3bc90cf88e6033a6f7eeade09b
Emine Avşar <mahinurk3438@gmail.com>
2025-09-30 00:13:36 +0300

    [IMDB-01] fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
-export default function page() {
-  return <div>Home</div>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY; // bu kod sunucu tarafında okunur bu yüzden güvenlidir
+//Client'a sızmaz.
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  console.log(data);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
