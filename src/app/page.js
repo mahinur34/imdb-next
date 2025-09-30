ae45702cc3834d49a173575a94542b92048b8282
Emine Avşar <mahinurk3438@gmail.com>
2025-09-30 17:23:07 +0300

    [IMDB-06] - Add Loading effect using nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,20 @@ const API_KEY = process.env.API_KEY; // bu kod sunucu tarafında okunur bu yüzd
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
 
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3${
+          genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        { next: { revalidate: 10000 } }
+      );
+      resolve(response);
+    }, 2000);
+  });
 
   const data = await res.json();
-  console.log(data);
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
